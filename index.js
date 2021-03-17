f1f71dfeb6ee4a74bb17f0e079ec4701ae24ce97
Anne Golladay <annegolladay@gmail.com>
2021-03-16 19:24:20 -0600

    completed task 7

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ console.log(computer);
 function game(user, computer){
     if(user === computer) {
       return `it's a tie`;
-    }else if(user === 'rock' && computer === 'scissors') {
+    }else if (user === 'rock' && computer === 'scissors') {
       return `you win!`;
     }else if (user === 'paper' && computer === 'rock') {
       return `you win!`;
@@ -270,10 +270,20 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score) {
+  if (score >= 90 && score <= 100) {
+    return 'you got an A';
+  } else if (score >= 80 && score <= 89) {
+    return 'you got a B';
+  } else if (score >= 70 && score <= 79) {
+    return 'you got a C';
+  } else if (score >= 60 && score <= 69) {
+    return 'you got a D';
+  } else if (score <= 59) {
+    return 'you got an F';
   }
-  
+}
+console.log(grade(59));
   
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
